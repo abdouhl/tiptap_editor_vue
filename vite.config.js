e8d72bfbd18550e0bb23d890131bb40c5b685c23
abdouhl <abderrahmane.36.hellal@gmail.com>
2024-04-28 09:27:05 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
   build: {
-      outDir: '../dist'
+      outDir: '../dist',
+      rollupOptions:{
+        input:{
+          main:resolve(__dirname,'src/index.html'),
+          app:resolve(__dirname,'src/app/index.html')
+        }
+      }
+  
   },
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '/app/': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
